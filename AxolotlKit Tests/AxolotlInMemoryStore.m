19b5a67b2c8fffee291eb652abe98274d3b7d7c7
Michael Kirk <michael.code@endoftheworl.de>
2017-03-28 21:55:29 -0400

    Add remaining nullability annotations to fix nullability warnings
    // FREEBIE

--- AxolotlKit Tests/AxolotlInMemoryStore.m
+++ AxolotlKit Tests/AxolotlInMemoryStore.m
@@ -1,9 +1,5 @@
 //
-//  AxolotlInMemoryStore.m
-//  AxolotlKit
-//
-//  Created by Frederic Jacobs on 17/10/14.
-//  Copyright (c) 2014 Frederic Jacobs. All rights reserved.
+//  Copyright (c) 2017 Open Whisper Systems. All rights reserved.
 //
 
 #import "AxolotlInMemoryStore.h"
@@ -56,7 +52,8 @@
     return [self.signedPreKeyStore objectForKey:[NSNumber numberWithInt:signedPreKeyId]];
 }
 
-- (NSArray *)loadSignedPreKeys{
+- (NSArray<SignedPreKeyRecord *> *)loadSignedPreKeys
+{
     NSMutableArray *results = [NSMutableArray array];
     
     for (SignedPreKeyRecord *signedPrekey in [self.signedPreKeyStore allValues]) {
@@ -177,4 +174,3 @@
 
 
 @end
-
