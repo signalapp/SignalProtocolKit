7619f1eede5eb8f672317e0ed6dc8dc375b9f00c
Matthew Chen <charlesmchen@gmail.com>
2018-01-22 16:49:38 -0500

    Add protocol context to protocol kit.

--- AxolotlKit/Classes/SPKAsserts.h
+++ AxolotlKit/Classes/SPKAsserts.h
@@ -1,31 +1,33 @@
 //
-//  Copyright (c) 2017 Open Whisper Systems. All rights reserved.
+//  Copyright (c) 2018 Open Whisper Systems. All rights reserved.
 //
+
+NS_ASSUME_NONNULL_BEGIN
 
 #ifndef SPKAssert
 
 #ifdef DEBUG
 
-#define USE_ASSERTS
+#define USE_SPK_ASSERTS
 
-#define CONVERT_TO_STRING(X) #X
-#define CONVERT_EXPR_TO_STRING(X) CONVERT_TO_STRING(X)
+#define SPK_CONVERT_TO_STRING(X) #X
+#define SPK_CONVERT_EXPR_TO_STRING(X) SPK_CONVERT_TO_STRING(X)
 
 // SPKAssert() and SPKFail() should be used in Obj-C methods.
 // SPKCAssert() and SPKCFail() should be used in free functions.
 
 #define SPKAssert(X)                                                                                                   \
     if (!(X)) {                                                                                                        \
-        DDLogError(@"%s Assertion failed: %s", __PRETTY_FUNCTION__, CONVERT_EXPR_TO_STRING(X));                        \
+        DDLogError(@"%s Assertion failed: %s", __PRETTY_FUNCTION__, SPK_CONVERT_EXPR_TO_STRING(X));                    \
         [DDLog flushLog];                                                                                              \
-        NSAssert(0, @"Assertion failed: %s", CONVERT_EXPR_TO_STRING(X));                                               \
+        NSAssert(0, @"Assertion failed: %s", SPK_CONVERT_EXPR_TO_STRING(X));                                           \
     }
 
 #define SPKCAssert(X)                                                                                                  \
     if (!(X)) {                                                                                                        \
-        DDLogError(@"%s Assertion failed: %s", __PRETTY_FUNCTION__, CONVERT_EXPR_TO_STRING(X));                        \
+        DDLogError(@"%s Assertion failed: %s", __PRETTY_FUNCTION__, SPK_CONVERT_EXPR_TO_STRING(X));                    \
         [DDLog flushLog];                                                                                              \
-        NSCAssert(0, @"Assertion failed: %s", CONVERT_EXPR_TO_STRING(X));                                              \
+        NSCAssert(0, @"Assertion failed: %s", SPK_CONVERT_EXPR_TO_STRING(X));                                          \
     }
 
 #define SPKFail(message, ...)                                                                                          \
@@ -70,3 +72,5 @@
 #endif
 
 #endif
+
+NS_ASSUME_NONNULL_END
