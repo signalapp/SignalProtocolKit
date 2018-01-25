51c30e935ce9ba58c445f7dab5dd680e5477c004
Matthew Chen <charlesmchen@gmail.com>
2018-01-25 16:40:13 -0500

    Add protocol context to protocol kit.

--- AxolotlKit/Classes/SessionCipher.m
+++ AxolotlKit/Classes/SessionCipher.m
@@ -157,7 +157,7 @@ NS_ASSUME_NONNULL_BEGIN
 
     SessionRecord *sessionRecord =
         [self.sessionStore loadSession:self.recipientId deviceId:self.deviceId protocolContext:protocolContext];
-    int unsignedPreKeyId         = [self.sessionBuilder processPrekeyWhisperMessage:preKeyWhisperMessage withSession:sessionRecord];
+    int unsignedPreKeyId         = [self.sessionBuilder processPrekeyWhisperMessage:preKeyWhisperMessage withSession:sessionRecord protocolContext:protocolContext];
     NSData *plaintext = [self decryptWithSessionRecord:sessionRecord
                                         whisperMessage:preKeyWhisperMessage.message
                                        protocolContext:protocolContext];
