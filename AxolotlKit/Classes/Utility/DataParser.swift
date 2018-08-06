6499f89c32bd62530a1d3a252c4aecbb682778e8
Matthew Chen <matthew@signal.org>
2018-08-06 17:01:08 -0400

    Migrate to Swift-based proto parsing.

--- AxolotlKit/Classes/Utility/DataParser.swift
+++ AxolotlKit/Classes/Utility/DataParser.swift
@@ -4,30 +4,35 @@
 
 import Foundation
 
-    public enum DataParserError : Error { case overflow(description : String) }
+public enum DataParserError : Error { case overflow(description : String) }
 
 // MARK: - DataParser
 
 @objc public class DataParser : NSObject {
-
-    fileprivate static let logTag = "DataParser" fileprivate let logTag = "DataParser"
-
-        private let data : Data private var cursor : Int
-                                                     = 0
-
-                                                     @objc public init(data
-                                                                       : Data)
+    
+    fileprivate static let logTag = "DataParser"
+    fileprivate let logTag = "DataParser"
+    
+    private let data : Data
+    private var cursor : Int = 0
+    
+    @objc public init(data: Data)
     {
         self.data = data
     }
-
+    
     @objc public func nextData(length : Int) throws->Data
     {
-        guard cursor + length
-            <= data.count else { throw DataParserError.overflow(description
-                                                                : "\(logTag) invalid data read") } let endIndex
-            = cursor + length let result = data.subdata(in
-                                                        : cursor..< endIndex) cursor
-            += length return result
+        guard cursor + length <= data.count else {
+            throw DataParserError.overflow(description
+                : "\(logTag) invalid data read")
+            
+        }
+        
+        let endIndex = cursor + length
+        let result = data.subdata(in : cursor..<endIndex)
+        cursor += length
+        return result
     }
 }
+
